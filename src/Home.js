381ef5d825d3687d7a966b824bd9d5b92fb8b7fe
Chamon562 <Chamon562@gmail.com>
2020-08-28 13:16:17 -0700

    [images] Add background image and tooth trasnparent gif

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ class Home extends Component {
     return (
       <div>
         <h1>Channee Dentistry</h1>
+      <img className='home' src={'https://www.animatedimages.org/data/media/151/animated-tooth-image-0013.gif'}/>
         <p>Where the dentist go for their dental needs</p>
       </div>
     );
