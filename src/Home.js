79193180402cf2dfe98ae707094bce327b27bd86
Chamon562 <Chamon562@gmail.com>
2020-08-28 10:52:41 -0700

    [home] Add Home Component

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// pass in COmponent because were making class component
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Channee Dentistry</h1>
+        <p>Where the dentist go for their dental needs</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
