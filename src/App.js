8a44f556fae63288c161ca91a4c1bf906277d413
Chamon562 <Chamon562@gmail.com>
2020-08-28 10:59:22 -0700

    [contact, proecures] Add contact and Procedures components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 import Home from "./Home";
+import Procedures from "./Procedures";
+import Contact from "./Contact";
 
 function App() {
   return (
     <div className="App">
+      {/* get react router dom need to install -done*/}
       {/* pass Home Component here */}
       <Home />
-      {/* get react router dom need to install */}
+      <Procedures />
+      <Contact />
     </div>
   );
 }
---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class Contact extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Contact Us</h1>
+                <p>Channee Math, MD</p>
+                <p>Email: Channee.Math@dentist.com</p>
+                <p>Phone: 555-555-5555</p>
+            </div>
+        )
+    }
+}
+
+export default Contact
