66df8b8b79e8eacb9d3fe39cd725cc2a1efe0236
Chamon562 <Chamon562@gmail.com>
2020-08-28 11:30:16 -0700

    [procedures] Add procedures as props into components

--- src/App.js
+++ src/App.js
@@ -9,14 +9,25 @@ import Procedures from "./Procedures";
 import Contact from "./Contact";
 
 function App() {
+// add some procedures before the return
+const procedures = [
+  'Deep Cleaning',
+  'Oral Surgery',
+  'Root Canal',
+  'Crown',
+  'Implants',
+  'Fillings'
+]
+
   return (
     // just cut and pasted routers that were in a div into the <Router><Home />
         // <Procedures />
         // <Contact />
+        // Router can only have one div that its refrenceing good prace to put in div
     <Router>
       <div className="App">
       <nav>
-      {/* inside Link tag key word to */}
+      {/* inside Link tag key word to  this adds a space{' '}*/}
         <Link to='/'>Home</Link>{' '}
         {/* <br/> */}
         <Link to='/procedures'>See our Procedures</Link>{' '}
@@ -28,8 +39,9 @@ function App() {
         {/* set each component as its own route rap entire return statement set up a router component*/}
         {/* route this is the path now pass in the component for that path */}
         {/* for every home route you have put in exact ikey worrd so it doesn thso whome */}
+        {/* took out component={Procedures}  and added         <Route path='/procedures'  render={() =><Procedures procedures={procedures} />} />*/}
         <Route exact path='/' component={Home} />
-        <Route path='/procedures' component={Procedures} />
+        <Route path='/procedures' render={() =><Procedures procedures={procedures} />} />
         <Route path='/contact' component={Contact} />
       </div>
     </Router>
--- src/Procedures.js
+++ src/Procedures.js
@@ -2,9 +2,15 @@ import React, { Component } from "react";
 
 class Procedures extends Component {
   render() {
+    //   make a map array
+    const procedursList = this.props.procedures.map((p, idx) =>{
+        return <li key={idx}>{p}</li>
+    })
+    console.log(this.props.procedures)
     return (
       <div>
         <h1>Procedures</h1>
+        {procedursList}
       </div>
     );
   }
